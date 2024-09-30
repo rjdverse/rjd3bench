116555f1b819132c696c8c8e32eb653630efb39f
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-09-30 13:08:04 +0200

    merge3

--- R/zzz.R
+++ R/zzz.R
@@ -4,14 +4,14 @@ NULL
 
 .onLoad <- function(libname, pkgname) {
 
-  if (!requireNamespace("rjd3toolkit", quietly = TRUE)) stop("Loading rjd3 libraries failed")
+    if (!requireNamespace("rjd3toolkit", quietly = TRUE)) stop("Loading rjd3 libraries failed")
 
-  result <- rJava::.jpackage(pkgname, lib.loc=libname)
-  if (!result) stop("Loading java packages failed")
+    result <- rJava::.jpackage(pkgname, lib.loc = libname)
+    if (!result) stop("Loading java packages failed")
 
-#  proto.dir <- system.file("proto", package = pkgname)
-#  RProtoBuf::readProtoFiles2(protoPath = proto.dir)
+    #  proto.dir <- system.file("proto", package = pkgname)
+    #  RProtoBuf::readProtoFiles2(protoPath = proto.dir)
 
-  # reload extractors
-  .jcall("jdplus/toolkit/base/api/information/InformationExtractors", "V", "reloadExtractors")
+    # reload extractors
+    .jcall("jdplus/toolkit/base/api/information/InformationExtractors", "V", "reloadExtractors")
 }
