d3864b3fa3be1c863e821cf5fe48f5b56da5f5b7
Corentin Lemasson <corentin.lemasson@nbb.be>
2026-07-13 11:41:35 +0200

    re-introducing multivariate chow-lin

--- R/tempdisagg.R
+++ R/tempdisagg.R
@@ -794,3 +794,319 @@ temporaldisaggregationI <- function(series, indicator,
     class(output) <- "JD3_TEMPDISAGGI_RSLTS"
     return(output)
 }
+
+
+
+
+#' @title Multivariate Temporal Disaggregaton of a System of Time Series by Regression Models.
+#'
+#' @description
+#' Performs simultaneous temporal disaggregation of a system of low frequency
+#' time series into higher frequency series, based on the multivariate
+#' extension of the Chow-Lin model or the Random Walk approach (Fernandez).
+#'
+#' @param series A named list of `ts` objects containing the low frequency time series to be disaggregated.
+#' @param constant Either a Boolean or a vector of Booleans. If a vector is provided, each element specifies whether a constant term is included in the model for each series, following the order in which they appear in the `series` object.
+#' The length of the the vector must match the number of series.
+#' If a single Boolean is provided (default if `TRUE`), it is applied to all series.
+#' Note that this argument is used only with Chow-Lin model (i.e., when `rhos` values are strictly less than 1). For further details, see the package vignette.
+#' @param trend Either a Boolean or a vector of Booleans. If a vector is provided, each element specifies whether a linear trend is included in the model for each series, following the order in which they appear in the `series` object.
+#' The length of the the vector must match the number of series.
+#' If a single Boolean is provided (default if `FALSE`), it is applied to all series.
+#' @param indicators a named list of `ts` objects or a named list of a list of `ts` objects. Each element represents one or more high-frequency indicator series associated with each series.
+#' If an element is `NULL`, no indicator is used for the corresponding series. The default value is `NULL`, meaning that no indicators are used for any series.
+#' @param ccseries A named list of `ts` objects containing the contemporaneous constraints. If `NULL` (the default), no contemporaneous constraints can be considered.
+#' @param ccdefinition A character vector defining each contemporaneous constraints. The elements of the vector must be written in the form \eqn{z=w_1 y_1+\ldots+w_n y_n} or \eqn{c=w_1 y_1+\ldots+w_n y_n} where:
+#' * \eqn{z} is the name of a contemporaneous constraint,
+#' * \eqn{(w_1,\ldots,w_n)} are optional numeric weights,
+#' * \eqn{(y_1,\ldots,y_n)} are the names of the time series and
+#' * \eqn{c} is a constant.
+#' The default is `NULL`, meaning that no contemporaneous constraint is considered.
+#' @param freq An integer giving the annual frequency of the disaggregated series.
+#' This argument is ignored when at least one indicator series is provided for any series.
+#' @param rhos Either a numeric value or a vector of numerics. If a vector is provided, each element specifies the value of the `rho` parameter associated to each series, following the order in which they appear in the `series` object.
+#' The length of the the vector must match the number of series.
+#' If a single numeric value is provided (default if `1`, corresponding to the Fernandez model), it is applied to all series.
+#' @param var A character string specifying the method used to estimate the variance-covariance matrix of the innovations.
+#' The default is `"fromUnivariate"`, meaning that is is estimated from the residuals of the univariate models. Others options include `"allEquals"`, which assume a diagonal matrix with identical variances (a strong assumption), and `"userDefined"`, where the matrix is supplied by the user via the `var.matrix` argument. For additional details, see the package vignette.
+#' @param var.matrix The variance-covariance matrix of the innovations.
+#' This argument is only used when `var = "userDefined"` and must be provided in that case.
+#'
+#' @return An object of class "JD3_MULTITEMPDISAGG_RSLTS" is returned. The following are returned
+#' invisibly as a list:
+#' * `regression` `[[1]]` regression coefficients for each series;
+#' * `estimation` `[[2]]` disaggregated Time-Series and standard deviation for each series, regression effects, smoothing part, parameter and variance-covariance matrix;
+#'
+#' @export
+#'
+#' @seealso `multivariatecholette()` for time series reconciliation.
+#'
+#' For more information, see the vignette:
+#'
+#' `utils::browseVignettes()`, e.g. `browseVignettes(package = "rjd3bench")`
+#'
+#' @examplesIf rjd3jars::check_java_version(silent = TRUE)
+#' # Low-frequency data
+#' Y1 <- ts(c(30.0, 30.6, 31.2, 31.6), frequency = 1, start = c(2010,1))
+#' Y2 <- ts(c(80.0, 81.2, 82.5, 82.6), frequency = 1, start = c(2010,1))
+#' Y3 <- ts(c(8.0, 8.1, 8.2, 8.2), frequency = 1, start = c(2010,1))
+#' lf_series <- list(y1 = Y1, y2 = Y2, y3 = Y3)
+#'
+#' # Contemporaneous constraint
+#' z <- ts(c(27.1,29.8,29.9,31.2,29.4,27.9,30.9,31.7,29.2,30.2,30.6,31.9,29.3,30.4,30.7,32.0), frequency = 4, start = c(2010,1))
+#'
+#' # High-frequency indicators
+#' x11 <- ts(c(7,7.2,8.1,7.5,8.5,7.8,8.1,8.4,8.6,7.8,8.0,8.3,8.7,7.9,8.0,8.6), frequency=4, start=c(2010,1))
+#' x12 <- ts(c(18,19.5,19.0,19.7,18.5,19.0,20.3,20.0,18.6,19.5,20.4,20.1,18.7,19.1,20.4,20.8), frequency = 4, start = c(2010,1))
+#' x2 <- NULL
+#' x3 <- ts(c(1.5,1.8,2,2.5,2.0,1.5,1.7,2.1,2.1,1.6,1.6,2.2,2.3,1.7,1.9,2.3), frequency = 4, start = c(2010,1))
+#' indic_series = list(y1 = list(x11, x12),
+#'                     y2 = NULL,
+#'                     y3 = x3)
+#'
+#' # Check consistency between temporal and contemporaneous constraints
+#' rowSums(cbind(Y1,Y2,Y3)) - stats::aggregate.ts(z) # ok!
+#'
+#' # Estimate models and get results
+#'
+#' ## Mix Chow-Lin - Fernandez
+#' rslt1 <- multivariatechowlin(series = lf_series,
+#'                              constant = c(FALSE, FALSE, TRUE),
+#'                              trend = c(FALSE, FALSE, FALSE),
+#'                              indicators = indic_series,
+#'                              ccseries = list(z = z),
+#'                              ccdefinition = "z=y1+y2+y3",
+#'                              freq = 4L,
+#'                              rhos = c(0.85, 1.0, 0.9),
+#'                              var = "fromUnivariate",
+#'                              var.matrix = NULL)
+#'
+#' d1 <- do.call(cbind, rslt1$estimation$disagg)
+#' ed1 <- do.call(cbind, rslt1$estimation$edisagg)
+#'
+#' ## Fernandez only (Random walk model) with user-defined variance-covariance matrix
+#' rslt2 <- multivariatechowlin(series = lf_series,
+#'                              constant = FALSE,
+#'                              trend = FALSE,
+#'                              indicators = indic_series,
+#'                              ccseries = list(z = z),
+#'                              ccdefinition = "z=y1+y2+y3",
+#'                              freq = 4L,
+#'                              rhos = 1.0,
+#'                              var = "userDefined",
+#'                              var.matrix = diag(c(0.003,0.01,0.001)))
+#'
+#' d2 <- do.call(cbind, rslt2$estimation$disagg)
+#' ed2 <- do.call(cbind, rslt2$estimation$edisagg)
+#'
+multivariatechowlin <- function(series,
+                                constant = TRUE,
+                                trend = FALSE,
+                                indicators = NULL,
+                                ccseries = NULL,
+                                ccdefinition = NULL,
+                                freq = 4L,
+                                rhos = 1,
+                                var = c("fromUnivariate", "allEquals", "userDefined"),
+                                var.matrix = NULL) {
+
+    var <- match.arg(var)
+
+    if(!is.null(var.matrix)) {
+        if (!is.matrix(var.matrix) || any(var.matrix[!diag(nrow(var.matrix), ncol(var.matrix))] != 0)) {
+            stop("Only diagonal variance covariance matrix of the innovations is currently supported.")
+        }
+    }
+
+    n <- length(series)
+    snames <- names(series)
+    are_lists <- is.list(series) && is.list(indicators)
+    same_size <- length(indicators) == n
+    same_names <- setequal(snames, names(indicators))
+    if (!are_lists || !same_size || !same_names) {
+        stop("The 'series' and 'indicators' arguments must be lists with matching size and names.")
+    }
+
+    # create the input
+    jdic_series <- .jnew("jdplus/toolkit/base/r/util/Dictionary")
+    for (i in seq_along(series)){
+        .jcall(jdic_series, "V", "add",
+               snames[i],
+               rjd3toolkit::.r2jd_tsdata(series[[i]]))
+    }
+
+    ncst <- length(constant)
+    if (ncst == 1) {
+        constant <- rep(constant, n)
+    } else if (ncst != n) {
+        stop("Size of 'constant' must be 1 or match number of series.")
+    }
+    jcst <- .jarray(as.logical(constant), contents.class = "Z")
+
+    ntrend <- length(trend)
+    if (ntrend == 1) {
+        trend <- rep(trend, n)
+    } else if (ntrend != n) {
+        stop("Size of 'trend' must be 1 or match number of series.")
+    }
+    jtrend <- .jarray(as.logical(trend), contents.class = "Z")
+
+    jarrdic_indic <- .jnew("jdplus/benchmarking/base/r/util/DictionaryGroups")
+    for (i in seq_along(indicators)){
+        if(!is.list(indicators[[i]])) indicators[[i]] <- list(x = indicators[[i]])
+
+        for(j in seq_along(indicators[[i]])){
+            indic <- indicators[[i]][[j]]
+
+            if (stats::is.ts(indic)) {
+                .jcall(jarrdic_indic, "V", "add",
+                       names(indicators)[i],
+                       rjd3toolkit::.r2jd_tsdata(indic))
+            } else if (is.null(indic)) {
+                .jcall(jarrdic_indic, "V", "add",
+                       names(indicators)[i],
+                       .jnull("jdplus/toolkit/base/api/timeseries/TsData"))
+            }
+        }
+    }
+
+    if (!is.null(ccseries)) {
+        if(!is.list(ccseries)) stop("'ccseries' must be a list.")
+        jdic_ccseries <- .jnew("jdplus/toolkit/base/r/util/Dictionary")
+        for (i in seq_along(ccseries)) {
+            .jcall(jdic_ccseries, "V", "add",
+                   names(ccseries)[i],
+                   rjd3toolkit::.r2jd_tsdata(ccseries[[i]]))
+        }
+    } else {
+        jdic_ccseries <- .jnull("jdplus/toolkit/base/r/util/Dictionary")
+    }
+
+    if (is.null(ccdefinition)) {
+        jccdef <- .jnull("[Ljava/lang/String;")
+    } else if (is.character(ccdefinition)) {
+        jccdef <- .jarray(as.character(ccdefinition), contents.class = "java/lang/String")
+    } else {
+        stop("'ccdefinition' must be NULL or a character vector.")
+    }
+
+    nrhos <- length(rhos)
+    if (nrhos == 1) {
+        rhos <- rep(rhos, n)
+    } else if (nrhos != n) {
+        stop("Size of 'rhos' must be 1 or match number of series.")
+    }
+    jrhos <- .jarray(as.numeric(rhos), contents.class = "D")
+
+    # fix constant if rho = 1 (diffuse initialization)
+    constant[rhos == 1] <- FALSE
+    jcst <- .jarray(as.logical(constant), contents.class = "Z")
+
+    jvar_mat <- rjd3toolkit::.r2jd_matrix(var.matrix)
+    jrslt <- .jcall(obj = "jdplus/benchmarking/base/r/TemporalDisaggregation",
+                    returnSig = "Ljdplus/benchmarking/base/api/multivariate/MultivariateChowLinResults;",
+                    method = "multiChowLin",
+                    jdic_series,
+                    jcst,
+                    jtrend,
+                    jarrdic_indic,
+                    jdic_ccseries,
+                    jccdef,
+                    as.integer(freq),
+                    jrhos,
+                    var,
+                    jvar_mat)
+
+    .jd2r_lhmap <- function(result, method, key, key.class = c("String"), value.class = c("TsData", "Matrix", "DoubleSeq", "List<String>")) {
+        key.class <- match.arg(key.class)
+        value.class <- match.arg(value.class)
+
+        jmap <- .jcall(result, "Ljava/util/Map;", method)
+
+        jkey <- .jnew("java/lang/String", key)
+        jobject <- .jcall(jmap, "Ljava/lang/Object;", "get", .jcast(jkey, "java/lang/Object"))
+
+        if(is.null(jobject)) return(NULL)
+
+        if (value.class == "TsData") {
+            out <- rjd3toolkit::.jd2r_tsdata(.jcast(jobject, "jdplus/toolkit/base/api/timeseries/TsData"))
+        } else if (value.class == "Matrix") {
+            out <- rjd3toolkit::.jd2r_matrix(jobject)
+        } else if (value.class == "DoubleSeq") {
+            out <- .jcall(jobject, "[D", "toArray")
+        } else if (value.class == "List<String>") {
+            jarr <- .jcall(jobject, "[Ljava/lang/Object;", "toArray")
+            out <- sapply(jarr, .jcall, "S", "toString")
+        }
+        return(out)
+    }
+
+    disagg <- edisagg <- regeffect <- reg <- regnames <- list()
+    model <- vector("list", length = n) |> setNames(snames)
+
+    for (i in seq_along(series)) {
+        sname <- snames[i]
+        disagg[[sname]] <- .jd2r_lhmap(jrslt,
+                                       "getDisaggregatedSeries",
+                                       sname,
+                                       value.class = "TsData")
+        edisagg[[sname]] <- .jd2r_lhmap(jrslt,
+                                        "getStdevDisaggregatedSeries",
+                                        sname,
+                                        value.class = "TsData")
+        regeffect[[sname]] <- .jd2r_lhmap(jrslt,
+                                          "getRegressionEffects",
+                                          sname,
+                                          value.class = "TsData")
+        reg[[sname]] <- .jd2r_lhmap(jrslt,
+                                    "getRegressors",
+                                    sname,
+                                    value.class = "Matrix")
+        coef <- .jd2r_lhmap(jrslt,
+                            "getCoefficients",
+                            sname,
+                            value.class = "DoubleSeq")
+
+        coef_var <- .jd2r_lhmap(jrslt,
+                                "getCoefficientsVariance",
+                                sname,
+                                value.class = "DoubleSeq")
+
+        reg_names <- .jd2r_lhmap(jrslt,
+                                 "getRegressorsNames",
+                                 sname,
+                                 value.class = "List<String>")
+
+        if(!is.null(coef)) {
+            se <- sqrt(coef_var)
+            t <- coef / se
+            model[[sname]] <- data.frame(coef, se, t, row.names = reg_names)
+        }
+    }
+
+    regression <- list(
+        type = ifelse(rhos == 1, "Rw", "Ar1"),
+        conversion = "Sum",
+        model = model
+    )
+
+    estimation <- list(
+        disagg = disagg,
+        edisagg = edisagg,
+        regeffect = regeffect,
+        smoothingpart =
+            rjd3toolkit::.jd3_object(jrslt,"MTD", TRUE) |>
+            rjd3toolkit::result("smoothingpart"),
+        parameters = rhos,
+        # residuals -> TODO
+        vcov = rjd3toolkit::.proc_matrix(jrslt, "innovationsvarcov")
+    )
+
+    output <- list(
+        regression = regression,
+        estimation = estimation
+    )
+    class(output) <- "JD3_MULTITEMPDISAGG_RSLTS"
+
+    return(output)
+}
